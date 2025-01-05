2e91c158fa067cf03265aafc9c73b3a025d66349
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-05 17:58:17 +0200

    reading request data

--- node-js-basics/app.ts
+++ node-js-basics/app.ts
@@ -1,5 +1,7 @@
+import { Buffer } from "node:buffer";
 import * as fs from "node:fs";
 import * as http from "node:http";
+import { parse } from "node:path";
 
 const server = http.createServer((req, res) => {
 	const url = req.url;
@@ -9,8 +11,10 @@ const server = http.createServer((req, res) => {
 		res.setHeader("Content-Type", "text/html");
 		res.write("<html>");
 		res.write("<body>");
-		res.write("<form action='message' method='POST'>");
-		res.write("<input type='text'/><button>Send Post Request</button>");
+		res.write("<form action='/message' method='POST'>");
+		res.write(
+			"<input type='text' name='message'/><button type='submit'>Send Post Request</button>"
+		);
 		res.write("</form>");
 		res.write("</body>");
 		res.write("</html>");
@@ -18,10 +22,33 @@ const server = http.createServer((req, res) => {
 	}
 
 	if (url === "/message" && method === "POST") {
-		fs.writeFileSync("message.txt", "response triggered");
+		console.log("req sent");
+		const body: any[] = [];
+
+		req.on("data", (chunk) => {
+			console.log("data listener triggered");
+			body.push(chunk);
+		});
+
+		req.on("end", () => {
+			console.log("end triggered");
+			const parsedBody = Buffer.concat(body).toString();
+			console.log(parsedBody);
+
+			fs.writeFileSync("message.txt", parsedBody);
+		});
+
+		
 		res.statusCode = 302;
 		res.setHeader("Location", "/");
+		return res.end();
 	}
+
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
+	res.write("<body><h1>Hello from The Deno Server</h1></body>");
+	res.write("</html>");
+	res.end();
 });
 
 server.listen(3000);
