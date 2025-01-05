18e2a7bb9889ba86002566f8b0d8c3f2f76e04bf
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-05 17:14:18 +0200

    listening to post request

--- node-js-basics/app.ts
+++ node-js-basics/app.ts
@@ -1,11 +1,27 @@
+import * as fs from "node:fs";
 import * as http from "node:http";
 
 const server = http.createServer((req, res) => {
-	res.setHeader("Content-Type", "text/html");
-	res.write("<html>");
-	res.write("<body><h1>Hello from The Deno Server</h1></body>");
-	res.write("</html>");
-	res.end();
+	const url = req.url;
+	const method = req.method;
+
+	if (url === "/") {
+		res.setHeader("Content-Type", "text/html");
+		res.write("<html>");
+		res.write("<body>");
+		res.write("<form action='message' method='POST'>");
+		res.write("<input type='text'/><button>Send Post Request</button>");
+		res.write("</form>");
+		res.write("</body>");
+		res.write("</html>");
+		return res.end();
+	}
+
+	if (url === "/message" && method === "POST") {
+		fs.writeFileSync("message.txt", "response triggered");
+		res.statusCode = 302;
+		res.setHeader("Location", "/");
+	}
 });
 
 server.listen(3000);
