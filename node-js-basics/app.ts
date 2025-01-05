c566142c59b69e29b0569738a9cb8bff685e10ae
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-05 16:55:28 +0200

    sending html response

--- node-js-basics/app.ts
+++ node-js-basics/app.ts
@@ -1,7 +1,11 @@
 import * as http from "node:http";
 
 const server = http.createServer((req, res) => {
-	console.log(req);
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
+	res.write("<body><h1>Hello from The Deno Server</h1></body>");
+	res.write("</html>");
+	res.end();
 });
 
 server.listen(3000);
