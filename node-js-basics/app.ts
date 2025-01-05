96d2215ec9a8af8f9fd5f7d527919fc18e658571
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-05 21:23:45 +0200

    created seperate routes file

--- node-js-basics/app.ts
+++ node-js-basics/app.ts
@@ -1,52 +1,6 @@
-import { Buffer } from "node:buffer";
-import * as fs from "node:fs";
 import * as http from "node:http";
+import requestHandler from "./routes.ts";
 
-const server = http.createServer((req, res) => {
-	const url = req.url;
-	const method = req.method;
-
-	if (url === "/") {
-		res.setHeader("Content-Type", "text/html");
-		res.write("<html>");
-		res.write("<body>");
-		res.write("<form action='/message' method='POST'>");
-		res.write(
-			"<input type='text' name='message'/><button type='submit'>Send Post Request</button>"
-		);
-		res.write("</form>");
-		res.write("</body>");
-		res.write("</html>");
-		return res.end();
-	}
-
-	if (url === "/message" && method === "POST") {
-		console.log("req sent");
-		const body: any[] = [];
-
-		req.on("data", (chunk) => {
-			console.log("data listener triggered");
-			body.push(chunk);
-		});
-
-		req.on("end", () => {
-			console.log("end triggered");
-			const parsedBody = Buffer.concat(body).toString();
-			console.log(parsedBody);
-
-			fs.writeFile("message.txt", parsedBody, (err) => {
-				res.statusCode = 302;
-				res.setHeader("Location", "/");
-				return res.end();
-			});
-		});
-	}
-
-	res.setHeader("Content-Type", "text/html");
-	res.write("<html>");
-	res.write("<body><h1>Hello from The Deno Server</h1></body>");
-	res.write("</html>");
-	res.end();
-});
+const server = http.createServer(requestHandler);
 
 server.listen(3000);
