c3e002b2282316edfcad3726dde18366e073b6cf
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-07 15:37:56 +0200

    Parsing request body

--- express-js/index.ts
+++ express-js/index.ts
@@ -1,15 +1,19 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log("App triggered");
-	next();
-});
+app.use(bodyParser.urlencoded());
 
 app.use("/results", (req, res, next) => {
-	console.log("First middleware");
+	console.log(req.body);
 	res.send("<h1>Results Page</h1>");
+});
+
+app.use("/submit", (req, res, next) => {
+	res.send(
+		"<form action='/results' method='POST'><input type='text' name='message'/><button type='submit'>Send Post Request</button></form>"
+	);
 });
 
 app.use("/", (req, res, next) => {
