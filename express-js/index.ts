e6d645919a6290268d6cd16c61b9aab3b2ca2394
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-07 14:59:54 +0200

    Added basic routing

--- express-js/index.ts
+++ express-js/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 
 const app = express();
+
 app.use((req, res, next) => {
-	console.log("First middleware");
+	console.log("App triggered");
 	next();
 });
 
-app.use((req, res, next) => {
+app.use("/results", (req, res, next) => {
+	console.log("First middleware");
+	res.send("<h1>Results Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
 	console.log("Second middleware");
 	res.send("<h1>Hello from Express!</h1>");
 });
