8d88679c8391c2acbcd2f4dab4997947c3f824a8
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-07 14:41:29 +0200

    middleware

--- express-js/index.ts
+++ express-js/index.ts
@@ -3,6 +3,15 @@ import * as http from "node:http";
 import express from "express";
 
 const app = express();
+app.use((req, res, next) => {
+	console.log("First middleware");
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log("Second middleware");
+	res.end();
+});
 
 const server = http.createServer(app);
 
