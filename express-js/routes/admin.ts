f17a1da7042cce48660430fff6054303f2fc687c
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-08 15:54:38 +0200

    Storing and sending data between views

--- express-js/routes/admin.ts
+++ express-js/routes/admin.ts
@@ -4,14 +4,18 @@ import { rootDir } from "../utils/path.ts";
 
 const router = Router();
 
+const products: any[] = [];
+
 router.post("/add-product", (req, res, next) => {
 	console.log(req.body);
+	// @ts-ignore
+	products.push({ title: req.body.title });
 	res.redirect("/");
-	// res.send("<h1>Results Page</h1>");
 });
 
 router.get("/add-product", (req, res, next) => {
 	res.sendFile(path.join(rootDir, "..", "views", "add-product.html"));
 });
 
+export { products };
 export default router;
--- express-js/routes/shop.ts
+++ express-js/routes/shop.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import path from "node:path";
 import { rootDir } from "../utils/path.ts";
+import { products } from "./admin.ts";
 
 const router = Router();
 
 router.get("/", (req, res, next) => {
+	console.log("products: ", products);
 	res.sendFile(path.join(rootDir, "..", "views", "shop.html"));
 });
 
